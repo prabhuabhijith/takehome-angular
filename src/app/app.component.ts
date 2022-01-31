fa2cccf477811b15f301c5f3e3104888bdc2b65b
Abhijith Prabhu <abhisprabhuks@gmail.com>
2022-01-31 10:57:36 -0500

    TakeHome test- Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
-
+  
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todos';
+  
+    public categories = [{name: '', timestamp: 0, todo:[{}]}]
+    public todoItems = [];
+    public selectedCategory;
+    public newTask;
+    public newCategory;
+
+    public addCategory() {
+        if (this.newCategory == '') {
+        }
+        else {
+            this.categories.push({name: this.newCategory, timestamp: new Date().getTime(), todo: [{}]})
+            this.newCategory = '';
+        }
+    }
+
+
+    public addToList() {
+      if (this.newTask == '') {
+      }
+      else {
+          this.selectedCategory.todo.push({name: this.newTask, completed: false});
+          this.newTask = '';
+      }
+  }
+
+  public deleteTask(index) {
+      this.selectedCategory.todo.splice(index, 1);
+  }
+
+  public setCategory(category) {
+    this.selectedCategory = this.categories.find((x) => x.timestamp === category.timestamp);
+  }
+
+  public completeTask(index){
+    this.selectedCategory.todo[index].completed = !this.selectedCategory.todo[index].completed;
+  }
 }
